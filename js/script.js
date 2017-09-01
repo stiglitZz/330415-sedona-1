c78180eb4c9364484dcf790fc027b383f223ea48
Artem <silvante90@gmail.com>
2017-09-01 18:06:06 +0300

    js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var link = document.querySelector(".search-btn");
+var popup = document.querySelector(".search-form");
+var test = document.querySelector(".site-logo");
+
+link.addEventListener("click", function(event) {
+
+      	event.preventDefault();
+      	popup.classList.toggle("search-form-hide");
+      	popup.classList.add("bounce");
+
+
+
+    });
+
